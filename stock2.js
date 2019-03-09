5b29b9e9634de10b11486ccedcc34173f26ca08a
Steve Cattanach <stevecatt@att.net>
2019-03-09 13:47:05 -0600

    stock with formula

--- stock2.js
+++ stock2.js
@@ -1,44 +1,56 @@
 let userInput = document.getElementById("userInput")
 let submitButton = document.getElementById("submit")
 let stockContainer = document.getElementById("stocks")
-let stockSelection = document.getElementById("selection").value
-
+//let stockSelection = document.getElementById("selection").value
+// this "stochChoice is  a parameter  and needs to permeate throught the function "
+function displayStocks(stockChoice){
+    let stockInfo = `
+    <div>
+      <label>STOCK NAME</label>
+      <p>${quotes[stockChoice].name}</p>
+      <label>PRICE</label>
+      <p>${quotes[stockChoice].price}</p>
+    </div>
+    `
+    stockContainer.innerHTML = stockInfo
+  }
+//this function uses the direct return from the getstockchoice function
+  function displayAltVersion(dog){
+      let stockInfo= `
+      <div>
+      <label>Stock Name</label>
+      <span>${dog.name}</span>
+      <span>${dog.price}</span>
+      `
+      stockContainer.innerHTML=stockInfo
+  }
 
 
 submitButton.addEventListener('click',function() {
     let stockSelection = document.getElementById("selection").value
     console.log(stockSelection)
     
-    window.refreshIntervalId
+   
+//this stockchoive is an argumant"
+    //let stockChoice = stockSelection
+   //stockchoice is now the argument passed into the getstockquote function
+    let updatedStock = getStockQuote(stockSelection)
+    //shows that i dont need to go to quotes  object to get the data 
+    console.log(updatedStock)
+    console.log(updatedStock.name)
+    console.log(updatedStock.price)
+    displayStocks(stockSelection)
 
-    let stockChoice = stockSelection
-
-  let stockInfo = `
-  <div>
-    <label>STOCK NAME</label>
-    <p>${quotes[stockChoice].name}</p>
-    <label>PRICE</label>
-    <p>${quotes[stockChoice].price}</p>
-  </div>
-  `
-  stockContainer.innerHTML = stockInfo
   
   let intervalId= window.setInterval(function() {
     submitButton.addEventListener('click',function() { 
         clearInterval(intervalId)})
 
-        getStockQuote(stockChoice)
+        getStockQuote(stockSelection)
+        displayStocks(stockSelection)
+        //displayAltVersion(updatedStock)
 
-  let stockInfo = `
-  <div>
-    <label>STOCK NAME</label>
-    <p>${quotes[stockChoice].name}</p>
-    <label>PRICE</label>
-    <p>${quotes[stockChoice].price}</p>
-  </div>
-  `
   
-  stockContainer.innerHTML = stockInfo
-  },4000)
+  },1000)
 })
 
